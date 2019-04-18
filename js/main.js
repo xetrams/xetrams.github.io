263c777d2e840e0812af2a62725782e6de63088c
Sasha Boykiv <boykiv@hotmail.com>
2019-04-18 11:09:57 +0300

    how we work page

--- js/main.js
+++ js/main.js
@@ -426,4 +426,260 @@ if($('.employee-list').length >0 ) {
 		}
 	});
 }
-$(".testimonials-section .owl-item").css("width", "100px");
+
+
+// METHODOLOGY STEPS
+
+if($('.scrum-section .metodology-steps-list').length >0 ) {
+	let benefits = $('.scrum-section .metodology-steps-list li');
+	let benefitsNum = benefits.length;
+
+	for (let i = 0; i < benefits.length; i++) {
+		if ($(benefits[i]).hasClass('active')) {
+			$(".active .block-content-wrapper").slideDown(0);
+		}
+	}
+
+	for (let i = 0; i < benefits.length; i++) {
+
+		$(benefits[i]).click(function(i) {
+			if (!$(this).hasClass('active')) {
+				for (let j = 0; j < benefitsNum; j++) {
+					$(benefits[j]).removeClass();
+					$(benefits[j]).find('.block-content-wrapper').slideUp(300);
+				}
+				$(this).addClass('active');
+				$(".active .block-content-wrapper").slideDown(300);
+			}
+		});
+	}
+}
+if($('.kanban-section .metodology-steps-list').length >0 ) {
+	let benefits = $('.kanban-section .metodology-steps-list li');
+	let benefitsNum = benefits.length;
+
+	for (let i = 0; i < benefits.length; i++) {
+		if ($(benefits[i]).hasClass('active')) {
+			$(".active .block-content-wrapper").slideDown(0);
+		}
+	}
+
+	for (let i = 0; i < benefits.length; i++) {
+
+		$(benefits[i]).click(function(i) {
+			if (!$(this).hasClass('active')) {
+				for (let j = 0; j < benefitsNum; j++) {
+					$(benefits[j]).removeClass();
+					$(benefits[j]).find('.block-content-wrapper').slideUp(300);
+				}
+				$(this).addClass('active');
+				$(".active .block-content-wrapper").slideDown(300);
+			}
+		});
+	}
+}
+if($('.waterfall-section .metodology-steps-list').length >0 ) {
+	let benefits = $('.waterfall-section .metodology-steps-list li');
+	let benefitsNum = benefits.length;
+
+	for (let i = 0; i < benefits.length; i++) {
+		if ($(benefits[i]).hasClass('active')) {
+			$(".active .block-content-wrapper").slideDown(0);
+		}
+	}
+
+	for (let i = 0; i < benefits.length; i++) {
+
+		$(benefits[i]).click(function(i) {
+			if (!$(this).hasClass('active')) {
+				for (let j = 0; j < benefitsNum; j++) {
+					$(benefits[j]).removeClass();
+					$(benefits[j]).find('.block-content-wrapper').slideUp(300);
+				}
+				$(this).addClass('active');
+				$(".active .block-content-wrapper").slideDown(300);
+			}
+		});
+	}
+}
+
+// METHODOLOGY GRAPH
+
+
+if($('.kanban-scheme').length > 0 ) {
+	let progressOffset = $('.kanban-scheme').offset().top;
+	let chartHeight = $('.kanban-scheme').height();
+	let progressEl = $('.kanban-scheme .horizontal-progress');
+	let progressFullWidth = $('.kanban-scheme .horizontal-bar').width() - 58;
+	let headerHeight = $('.main-header').height();
+	let vindowHeight = $(window).height();
+
+	$(window).scroll(function (event) {
+		let scroll = $(window).scrollTop();
+		let progressHeight = scroll - progressOffset + $(window).height() - chartHeight;
+		let progressPercents = (progressHeight / (vindowHeight - headerHeight - chartHeight));
+		if (progressPercents > 1) {
+			progressPercents = 1;
+		}
+		let progressTotal = 0;
+		if (progressHeight > 0) {
+			progressTotal = progressPercents*progressFullWidth;
+		} else {
+			progressTotal = 0;
+		}
+		$(progressEl).width(progressTotal);
+		
+
+		let steps = $('.kanban-scheme .list-metodology-scheme div');
+		let stepsLeft = [];
+
+		for (let i = 0; i < steps.length; i++) {
+			stepsLeft.push(parseInt($(steps[i]).css('left')));
+		}
+
+		for (let i = 0; i < steps.length; i++) {
+			if (stepsLeft[i] - progressTotal < 0) {
+				$(steps[i]).addClass("active");
+			} else {
+				$(steps[i]).removeClass("active");
+			}
+		}
+	});
+}
+
+if($('.waterfall-scheme').length > 0 ) {
+	let progressOffset = $('.waterfall-scheme').offset().top;
+	let chartHeight = $('.waterfall-scheme').height();
+	let progressEl = $('.waterfall-scheme .horizontal-progress');
+	let progressFullWidth = $('.waterfall-scheme .horizontal-bar').width() - 58;
+	let headerHeight = $('.main-header').height();
+	let vindowHeight = $(window).height();
+
+	$(window).scroll(function (event) {
+		let scroll = $(window).scrollTop();
+		let progressHeight = scroll - progressOffset + $(window).height() - chartHeight;
+		let progressPercents = (progressHeight / (vindowHeight - headerHeight - chartHeight));
+		if (progressPercents > 1) {
+			progressPercents = 1;
+		}
+		let progressTotal = 0;
+		if (progressHeight > 0) {
+			progressTotal = progressPercents*progressFullWidth;
+		} else {
+			progressTotal = 0;
+		}
+		$(progressEl).width(progressTotal);
+		
+
+		let steps = $('.waterfall-scheme .list-metodology-scheme div');
+		let stepsLeft = [];
+
+		for (let i = 0; i < steps.length; i++) {
+			stepsLeft.push(parseInt($(steps[i]).css('left')));
+		}
+
+		for (let i = 0; i < steps.length; i++) {
+			if (stepsLeft[i] - progressTotal < 0) {
+				$(steps[i]).addClass("active");
+			} else {
+				$(steps[i]).removeClass("active");
+			}
+		}
+	});
+}
+
+let scrumUnit = 150;
+
+if($('.scrum-first-line').length > 0 ) {
+	let progressOffset = $('.scrum-first-line').offset().top;
+	let chartHeight = $('.scrum-first-line').height();
+	let progressEl = $('.scrum-first-line .horizontal-progress');
+	let progressFullWidth = $('.scrum-first-line .horizontal-bar').width() - 58;
+
+	$(window).scroll(function (event) {
+		let scroll = $(window).scrollTop();
+		let progressHeight = scroll - progressOffset + $(window).height() - chartHeight;
+		let progressPercents = progressHeight / scrumUnit;
+		if (progressPercents > 1) {
+			progressPercents = 1;
+			$('.scrum-scheme .circle').addClass("active");
+		} else {
+			$('.scrum-scheme .circle').removeClass("active");
+		}
+		let progressTotal = 0;
+		if (progressHeight > 0) {
+			progressTotal = progressPercents*progressFullWidth;
+		} else {
+			progressTotal = 0;
+		}
+		$(progressEl).width(progressTotal);
+		
+
+		let steps = $('.scrum-first-line .list-metodology-scheme div');
+		let stepsLeft = [];
+
+		for (let i = 0; i < steps.length; i++) {
+			stepsLeft.push(parseInt($(steps[i]).css('left')));
+		}
+
+		for (let i = 0; i < steps.length; i++) {
+			if (stepsLeft[i] - progressTotal < 0) {
+				$(steps[i]).addClass("active");
+			} else {
+				$(steps[i]).removeClass("active");
+			}
+		}
+	});
+}
+
+if($('.scrum-scheme .circle').length > 0 ) {
+	let progressOffset = $('.scrum-first-line').offset().top;
+	let chartHeight = $('.scrum-first-line').height();
+	let progressEl = $('.scrum-first-line .horizontal-progress');
+	let progressFullWidth = $('.scrum-first-line .horizontal-bar').width() - 58;
+
+	$(window).scroll(function (event) {
+		let scroll = $(window).scrollTop();
+		let progressHeight = scroll - progressOffset + $(window).height() - chartHeight;
+		let progressPercents = progressHeight / scrumUnit;
+	});
+}
+
+if($('.scrum-second-line').length > 0 ) {
+	let progressOffset = $('.scrum-second-line').offset().top;
+	let chartHeight = $('.scrum-second-line').height();
+	let progressEl = $('.scrum-second-line .horizontal-progress');
+	let progressFullWidth = $('.scrum-second-line .horizontal-bar').width() - 58;
+
+	$(window).scroll(function (event) {
+		let scroll = $(window).scrollTop();
+		let progressHeight = scroll - progressOffset + $(window).height() - chartHeight;
+		let progressPercents = progressHeight / scrumUnit;
+		if (progressPercents > 1) {
+			progressPercents = 1;
+		}
+		let progressTotal = 0;
+		if (progressHeight > 0) {
+			progressTotal = progressPercents*progressFullWidth;
+		} else {
+			progressTotal = 0;
+		}
+		$(progressEl).width(progressTotal);
+		
+
+		let steps = $('.scrum-second-line .list-metodology-scheme div');
+		let stepsLeft = [];
+
+		for (let i = 0; i < steps.length; i++) {
+			stepsLeft.push(parseInt($(steps[i]).css('left')));
+		}
+
+		for (let i = 0; i < steps.length; i++) {
+			if (stepsLeft[i] - progressTotal < 0) {
+				$(steps[i]).addClass("active");
+			} else {
+				$(steps[i]).removeClass("active");
+			}
+		}
+	});
+}
